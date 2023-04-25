bf4fa8470cbf1d0953d4644e3766e33607d2f3f7
Danylo Andrieiev <danandryeyev@gmail.com>
2023-04-25 15:17:45 +0300

    Implement layout for partner search screen

--- feature/partner-search-ui/src/main/java/com/coursework/speakoutchat/partner_search_ui/PartnerSearchFragment.kt
+++ feature/partner-search-ui/src/main/java/com/coursework/speakoutchat/partner_search_ui/PartnerSearchFragment.kt
@@ -2,5 +2,5 @@ package com.coursework.speakoutchat.partner_search_ui
 
 import androidx.fragment.app.Fragment
 
-class PartnerSearchFragment : Fragment() {
+class PartnerSearchFragment : Fragment(R.layout.fragment_partner_search) {
 }
